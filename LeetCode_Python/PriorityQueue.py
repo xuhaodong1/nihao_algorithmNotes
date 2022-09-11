f08bf35f387a8f31e705634cc1727575a305940d
xuhaodong1 <xuhaodong1011@qq.com>
2022-09-11 22:04:39 +0800

    @dev #添加注释

--- LeetCode_Python/PriorityQueue.py
+++ LeetCode_Python/PriorityQueue.py
@@ -7,12 +7,12 @@ class PriorityQueue:
     # 题目链接：[857. 雇佣 K 名工人的最低成本](https://leetcode.cn/problems/minimum-cost-to-hire-k-workers/)
     def mincostToHireWorkers(self, quality, wage, k) -> float:
         qw = sorted(zip(quality, wage), key=lambda p: p[1] / p[0]) #按照 wage[i] / quality[i] 排序
-        h = [-q for q, _ in qw[:k]]
+        h = [-q for q, _ in qw[:k]] # 加负号变成最大堆
         heapify(h)
         sum_q = -sum(h)
-        ans = sum_q * qw[k - 1][1] / qw[k - 1][0]
-        for q, w in qw[k:]: # 之前的按照单位 qw[k - 1][1] / qw[k - 1][0] 处理
-            if q < -h[0]:
+        ans = sum_q * qw[k - 1][1] / qw[k - 1][0] # 选 w / q 值最小的 k 名工人组成当前的最优解
+        for q, w in qw[k:]:
+            if q < -h[0]: # sum_q 可以变小，从而可能得到更优的答案, 而 w / q 是递增的只会变大
                 sum_q += heapreplace(h, -q) + q
                 ans = min(ans, sum_q * w / q)
         return ans
