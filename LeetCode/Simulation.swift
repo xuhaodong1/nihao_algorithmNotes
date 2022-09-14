49658e84687fa86ab659027772c5127ee2e94ed5
xuhaodong1 <xuhaodong1011@qq.com>
2022-09-14 10:04:27 +0800

    @append 1619. 删除某些元素后的数组均值

--- LeetCode/Simulation.swift
+++ LeetCode/Simulation.swift
@@ -43,11 +43,18 @@ class Simulation: BaseCode {
         }
     }
 
+    /// 题目链接：[1619. 删除某些元素后的数组均值](https://leetcode.cn/problems/mean-of-array-after-removing-some-elements/)
+    func trimMean(_ arr: [Int]) -> Double {
+        let n = arr.count, removeCnt = Int(Double(n) * 0.05)
+        return Double(arr.sorted()[removeCnt..<n-removeCnt].reduce(0, +)) / Double(n - removeCnt * 2)
+    }
+
 //    override var excuteable: Bool {
 //        return true
 //    }
 
     override func executeTestCode() {
         super.executeTestCode()
+        print(trimMean([6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]))
     }
 }
