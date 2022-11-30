879bde6156eb0db98026be29559c1e5a8b39c1f8
xuhaodong1 <xuhaodong1011@qq.com>
2022-11-30 10:08:33 +0800

    @dev #append 895

--- LeetCode/Stack.swift
+++ LeetCode/Stack.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+/// 题目链接：[895. 最大频率栈](https://leetcode.cn/problems/maximum-frequency-stack/description/)
+class FreqStack {
+    var cnts = [Int: Int]() // 数字的出现频率
+    var stacks = [[Int]]() // 以出现次数相同的组成的序列, stacks[i] 表示出现次数为 i + 1 组成的序列
+    init() { }
+    func push(_ val: Int) {
+        var cnt = cnts[val, default: 0]
+        if cnt == stacks.count { stacks.append([]) } // 有一个新的cnt
+        stacks[cnt].append(val)
+        cnts[val, default: 0] += 1
+    }
+
+    func pop() -> Int {
+        let back = stacks.count - 1
+        let val = stacks[back].removeLast()
+        if stacks[back].isEmpty { stacks.removeLast() }
+        cnts[val, default: 0] -= 1
+        return val
+    }
+}
+
+
 /// 栈等相关练习题
 class Stack: BaseCode {
 
